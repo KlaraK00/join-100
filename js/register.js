21b14dd26fff741e392c7707175afa1cf0a9b28b
KlaraK00 <klara.kneisl.2000@gmail.com>
2024-03-01 15:24:18 +0100

    corrected password images

--- js/register.js
+++ js/register.js
@@ -1,17 +1,5 @@
 /* ---------- "checkbox" ---------- */
 
-// let registerCheckbox = document.getElementById('registerCheckbox');
-
-// if (registerCheckbox) {
-//     registerCheckbox.addEventListener('change', () => {
-//         if (registerCheckbox.checked) {
-//             registerShowCheckedCheckbox();
-//         } else {
-//             registerShowNoCheckedCheckbox();
-//         }
-//     })
-// }
-
 function changeCheckbox(id) {
     let checkbox = document.getElementById(id);
     if (checkbox.src.includes('registerCheckedCheckbox.png')) {
@@ -19,27 +7,4 @@ function changeCheckbox(id) {
     } else {
         checkbox.src = './img/registerCheckedCheckbox.png';
     }
-}
-
-
-function registerShowCheckedCheckbox() {
-    let registerCheckedCheckbox = document.getElementById('registerCheckedCheckbox');
-    let registerCheckbox = document.getElementById('registerCheckbox');
-    // let registerSpanAccept = document.getElementById('registerSpanAccept');
-
-    registerCheckedCheckbox.classList.remove('d-none');
-    registerCheckbox.classList.add('d-none');
-    registerCheckedCheckbox.style.display = 'inline-block';
-    // registerSpanAccept.classList.add('marLeft23');
-}
-
-function registerShowNoCheckedCheckbox() {
-    let registerCheckedCheckbox = document.getElementById('registerCheckedCheckbox');
-    let registerCheckbox = document.getElementById('registerCheckbox');
-    // let registerSpanAccept = document.getElementById('registerSpanAccept');
-
-    registerCheckedCheckbox.classList.add('d-none');
-    registerCheckbox.classList.remove('d-none');
-    // registerSpanAccept.classList.remove('marLeft23');
-    registerCheckedCheckbox.style.display = 'none';
 }
