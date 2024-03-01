9f93f7444140a3b8b6982404f0dc20cc549f76d6
ChrisZ95 <160311410+ChrisZ95@users.noreply.github.com>
2024-03-01 15:47:45 +0100

    Edit Contact Button

--- js/contacts.js
+++ js/contacts.js
@@ -5,11 +5,30 @@ function addNewContact(){
     document.getElementById('overlay-add-contact').innerHTML = createNewContactHTML();
 }
 
+function editContact(){
+    document.getElementById('overlay-edit-contact').classList.remove('d-none');
+    document.getElementById('overlay-edit-contact').classList.add('d-flex');
+    document.getElementById('overlay-edit-contact').classList.add('overlay-add-contact');
+    document.getElementById('overlay-edit-contact').innerHTML = createEditContactHTML();
+}
+
+function closeNewContactWindow(){
+    document.getElementById('overlay-add-contact').classList.add('d-none');
+    document.getElementById('overlay-add-contact').classList.remove('d-flex');
+    document.getElementById('overlay-add-contact').classList.remove('overlay-add-contact');
+}
+
+function closeEditContactWindow(){
+    document.getElementById('overlay-edit-contact').classList.add('d-none');
+    document.getElementById('overlay-edit-contact').classList.remove('d-flex');
+    document.getElementById('overlay-edit-contact').classList.remove('overlay-add-contact');
+}
+
 function createNewContactHTML() {
     return `
     <div class="overlay-contact">
         <div class="close-button">
-            <img onclick="closeWindow()" src="./img/Close.png">
+            <img onclick="closeNewContactWindow()" src="./img/Close.png">
         </div>
         <div class="blue-container">
             <img src="./img/popup-join-logo.png">
@@ -36,8 +55,32 @@ function createNewContactHTML() {
     `;
 }
 
-function closeWindow(){
-    document.getElementById('overlay-add-contact').classList.add('d-none');
-    document.getElementById('overlay-add-contact').classList.remove('d-flex');
-    document.getElementById('overlay-add-contact').classList.remove('overlay-add-contact');
+function createEditContactHTML() {
+    return `
+    <div class="overlay-contact">
+        <div class="close-button">
+            <img onclick="closeEditContactWindow()" src="./img/Close.png">
+        </div>
+        <div class="blue-container">
+            <img src="./img/popup-join-logo.png">
+            <h1>Edit contact</h1>
+            <img src="./img/blue-underline.png">
+        </div>
+        <div class="profile-logo">
+            <img src="./img/profile-logo.png">
+        </div>
+        <div class="input-new-contact">
+            <form>
+                <div class="input-fields">
+                    <input class="input background-img-profile" placeholder="Name">
+                    <input class="input background-img-mail" placeholder="Email">
+                    <input class="input background-img-phone" placeholder="Phone">
+                </div>
+                <div class="add-contact-button">
+                    <button class="button4">Delete</button>
+                    <button class="button5">Save</button>
+                </div>
+            </form>
+    </div>
+    `;
 }
