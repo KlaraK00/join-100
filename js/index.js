caccb6e7cc032e3a93def846810d781b3012670a
KlaraK00 <klara.kneisl.2000@gmail.com>
2024-03-02 21:05:56 +0100

    log in function first commit

--- js/index.js
+++ js/index.js
@@ -6,7 +6,23 @@ async function initLogIn() {
 
 /* ---------- log in ---------- */
 
-function logIn() {
-    let logInEmail = document.getElementById('logInEmail');
-    let logInPassword = document.getElementById('logInPassword');
+function logIn(event) {
+    noReload(event);
+    let logInEmail = document.getElementById('logInEmail').value;
+    let logInPassword = document.getElementById('logInPassword').value;
+    let indexOfEmail = users.findIndex(user => user.email == logInEmail);
+    let indexOfPassword = users.findIndex(user => user.password == logInPassword);
+    if(userIsFound(indexOfEmail, indexOfPassword)) {
+        console.log('user is found!', users[indexOfEmail]);
+        // CURRENT USER: guest-login = firstName: Guest, lastName: '' / user-login = users[i] => both cases in LOCAL STORAGE!
+        // guest-login = G / user-login = first letters of names
+    }
+}
+
+function userIsFound(email, password) {
+    if(email == password) {
+        return true;
+    } else {
+        return false;
+    }
 }
