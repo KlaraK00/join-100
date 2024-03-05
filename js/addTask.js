4fd3c64af75aac120ac6c6e0284061eb0a6a7f6b
davsw1905 <148264644+davsw1905@users.noreply.github.com>
2024-03-05 20:32:44 +0100

    restructured Code for better maintenance

--- js/addTask.js
+++ js/addTask.js
@@ -1,31 +1,45 @@
 let tasks = [];
 
-function createTask() {
-  let title = document.getElementById("title").value;
-  let description = document.getElementById("description").value;
-  let assignedTo = document.getElementById("assign").value; 
-  let dueDate = document.getElementById("due").value;
-  let priority = getPriority();
-  let category = document.getElementById("category").value;
-  let subtasks = document.getElementById("subtasks").value;
-
-  let task = {
-    title: title,
-    description: description,
-    assignedTo: assignedTo,
-    dueDate: dueDate,
-    priority: priority,
-    category: category,
-    subtasks: subtasks,
-  };
-
-  console.log(task);
-
-  tasks.push(task);
-
-  let tasksJSON = JSON.stringify(tasks);
-  console.log(tasksJSON);
+// Function to retrieve input value
+function getInputValue(id) {
+    return document.getElementById(id).value;
 }
+
+// Function to create a task object
+function createTaskObject(title, description, assignedTo, dueDate, priority, category, subtasks) {
+    return {
+        title,
+        description,
+        assignedTo,
+        dueDate,
+        priority,
+        category,
+        subtasks,
+    };
+}
+
+
+function pushTask(task) {
+    console.log(task);
+    tasks.push(task); 
+    console.log(JSON.stringify(tasks));
+}
+
+// The main function to create a task
+function createTask() {
+    let title = getInputValue("title");
+    let description = getInputValue("description");
+    let assignedTo = getInputValue("assign");
+    let dueDate = getInputValue("due");
+    let priority = getPriority(); 
+    let category = getInputValue("category");
+    let subtasks = getInputValue("subtasks");
+
+    let task = createTaskObject(title, description, assignedTo, dueDate, priority, category, subtasks);
+
+    pushTask(task);
+}
+
 function getPriority() {
     //access all prio buttons
     let priorityButtons = document.querySelectorAll('.prio button'); 
