db0ee63ddb49bdd16e0afd5964d7781c0d1f176e
davsw1905 <148264644+davsw1905@users.noreply.github.com>
2024-03-05 20:24:03 +0100

    priority buttons are now properly working and being pushed into the array

--- js/addTask.js
+++ js/addTask.js
@@ -1,36 +1,52 @@
-
 let tasks = [];
+
 function createTask() {
-    // Retrieve values from HTML elements
-    let title = document.getElementById('title').value;
-    let description = document.getElementById('description').value;
-    let assignedTo = document.getElementById('assign').value; // Added ID for Assigned To field
-    let dueDate = document.getElementById('due').value;
-    let priority = getPriority();
-    let category = document.getElementById('category').value;
-    let subtasks = document.getElementById('subtasks').value;
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  let assignedTo = document.getElementById("assign").value; 
+  let dueDate = document.getElementById("due").value;
+  let priority = getPriority();
+  let category = document.getElementById("category").value;
+  let subtasks = document.getElementById("subtasks").value;
 
-    // Create a task object
-    let task = {
-        title: title,
-        description: description,
-        assignedTo: assignedTo,
-        dueDate: dueDate,
-        priority: priority,
-        category: category,
-        subtasks: subtasks
-    };
+  let task = {
+    title: title,
+    description: description,
+    assignedTo: assignedTo,
+    dueDate: dueDate,
+    priority: priority,
+    category: category,
+    subtasks: subtasks,
+  };
 
-    
+  console.log(task);
 
-    // Perform further actions with the task object, such as adding it to an array, sending it to a server, etc.
-    console.log(task); // For demonstration, log the task object to the console
+  tasks.push(task);
 
-    // Push the task object into the tasks array
-    tasks.push(task);
-
-    // Optionally, convert the array to JSON format
-    let tasksJSON = JSON.stringify(tasks);
-    console.log(tasksJSON);
-    
+  let tasksJSON = JSON.stringify(tasks);
+  console.log(tasksJSON);
 }
+function getPriority() {
+    //access all prio buttons
+    let priorityButtons = document.querySelectorAll('.prio button'); 
+    //loop through buttons
+    for (let button of priorityButtons) {
+        //check for active class
+        if (button.classList.contains('active')) { 
+            return button.textContent.trim(); 
+        }
+    }
+    return "Not set"; 
+}
+
+function activateButton(buttonId) {
+    //remove active from all buttons
+    document.querySelectorAll('#priority .prio button').forEach(button => {
+        button.classList.remove('active');
+    });
+
+    //add active to the button the user clicks on 
+    let button = document.getElementById(buttonId);
+    button.classList.add('active');
+}
+
