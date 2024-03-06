e52bd0669eb06fe44399d0294220d43f1539a19e
unknown <lucas.liebert20@gmail.com>
2024-03-06 13:39:36 +0100

    greeting Time

--- js/summary.js
+++ js/summary.js
@@ -7,12 +7,35 @@ function start() {
 }
 
 /**
- * Greets the current user by displaying their first name.
+ * Greets the current user by displaying their first name along with a greeting based on the time of the day.
  */
 function greetCurrentUser() {
   /**
-   * @type {HTMLElement} greetingNameContainer - The HTML container for displaying the greeting.
+   * @type {HTMLElement} greetingContainer - The HTML container for displaying the greeting.
    */
-  let greetingNameContainer = document.getElementById("greetingNameContainer");
-  greetingNameContainer.innerHTML = getCurrentUser().firstName;
+  let greetingContainer = document.getElementById("greeting");
+  let greeting = generateGreeting();
+  let nameContainer = document.getElementById("greetingNameContainer");
+  nameContainer.textContent = getCurrentUser().firstName;
+  greetingContainer.textContent = greeting;
+}
+
+/**
+ * Generates a greeting based on the time of the day.
+ * @returns {string} The generated greeting.
+ */
+function generateGreeting() {
+  let date = new Date();
+  let hour = date.getHours();
+  let greeting = "";
+  
+  if (hour < 12) {
+    greeting = "Good morning, ";
+  } else if (hour < 18) {
+    greeting = "Good afternoon, ";
+  } else {
+    greeting = "Good evening, ";
+  }
+
+  return greeting;
 }
