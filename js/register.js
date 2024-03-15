f5846f37508f3ebe0965fd23af7551c1dd0e4907
KlaraK00 <klara.kneisl.2000@gmail.com>
2024-03-15 12:00:03 +0100

    a little bit of JSDoc in register.js

--- js/register.js
+++ js/register.js
@@ -19,8 +19,12 @@ let colors = [
     'rgba(255,70,70,255)'
 ]
 
-/* ---------- init ---------- */
+/* ---------- INIT-FUNCTION ---------- */
 
+/**
+ * This function initializes the registration process by setting the loggedIn-variable to false.
+ * It is loading the logged-in-status, users and contacts asynchronously.
+ */
 async function initRegister() {
     setLoggedInFalse();
     loadLoggedIn();
@@ -28,31 +32,54 @@ async function initRegister() {
     await loadContacts();
 }
 
+/**
+ * This function stores an item in the browser's local storage with the key "loggedIn" and the value "false".
+ */
 function setLoggedInFalse() {
     loggedIn = false;
     setLocalStorageItem('loggedIn', loggedIn);
 }
 
+/**
+ * This function sets the variable "loggedIn" if it exists in the local storage under the same name.
+ */
 function loadLoggedIn() {
     if (loggedInExists()) {
         loggedIn = getLoggedIn();
     }
 }
 
+/**
+ * This function checks if a value under the key "loggedIn" exists in the browser's local storage.
+ * 
+ * @returns {boolean} - Returns true or false, if the value of the key "loggedIn" is true or false.
+ */
 function loggedInExists() {
     return getLoggedIn() === false || getLoggedIn() === true;
 }
 
+/**
+ * This function returns the value with the key 'loggedIn' from the local storage.
+ * 
+ * @returns {object || string || null} - Returns an object or a string when the key is found and null when the key doesn't exist in the local storage.
+ */
 function getLoggedIn() {
     return getLocalStorageItem('loggedIn');
 }
 
+/**
+ * This function loads user data asynchronously and sets the variable 'users' if there is an item in the remote storage with the key 'users'.
+ */
 async function loadUsers() {
     if(await usersExist()) {
         users = JSON.parse(await getItem('users'));
     }
 }
 
+/**
+ * This function is used to check if there is an item in the remote storage with the key 'users'.
+ * @returns {object || null} - Returns an object, if the key 'users' is found or null if the key 'users' isn't found.
+ */
 async function usersExist() {
     return getItem('users');
 }
